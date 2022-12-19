6423498ab6140305da0c8d28deffad0e4938a023
Soheil Pasbakhsh <mssoheil88@yahoo.com>
2022-12-19 20:24:16 +0330

    feat: add form

--- js/index.js
+++ js/index.js
@@ -15,3 +15,13 @@ function handleTodoDone(target) {
   target.innerHTML = "";
   target.appendChild(newElement);
 }
+
+const toastTrigger = document.getElementById("liveToastBtn");
+const toastLiveExample = document.getElementById("liveToast");
+if (toastTrigger) {
+  toastTrigger.addEventListener("click", () => {
+    const toast = new bootstrap.Toast(toastLiveExample);
+
+    toast.show();
+  });
+}
